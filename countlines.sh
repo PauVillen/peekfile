1aa0bc6813d1c1a1fb74c4c3776f39fb2417bb10
Pau Villén <pauvillenvidal@gmail.com>
2025-11-05 19:21:36 +0100

    updated countlines file

--- countlines.sh
+++ countlines.sh
@@ -1,10 +1,11 @@
-file="$1"
-num_lines=$(wc -l < "$file")
+for file in "$@"; do
+  num_lines=$(wc -l < "$file")
 
-if (( num_lines == 0 )); then
-  echo "$file has 0 lines."
-elif (( num_lines == 1 )); then
-  echo "$file has 1 line."
-else
-  echo "$file has $num_lines lines."
-fi
+  if (( num_lines == 0 )); then
+    echo "$file has 0 lines."
+  elif (( num_lines == 1 )); then
+    echo "$file has 1 line."
+  else
+    echo "$file has $num_lines lines."
+  fi
+done
