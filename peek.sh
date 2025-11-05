12fe18b29d522eaa92c25b6562c4f381daa2cd0e
Pau Villén <pauvillenvidal@gmail.com>
2025-11-05 19:13:14 +0100

    modification to peek.sh (homework for day 10)

--- peek.sh
+++ peek.sh
@@ -1,5 +1,24 @@
 file="$1"
 lines="$2"
-head -n "$lines" "$file"
-echo "..."
-tail -n "$lines" "$file"
+
+if [[ -z "$lines" ]]; then
+	lines=3
+fi
+
+total_lines=$(wc -l > "$file")
+
+if (( total_lines <= 2 * lines); then
+	cat "$file"
+else
+	echo "Warning: File has $total_lines lines, showing only first and last $lines lines."
+ 	head -n "$lines" "$file"
+	echo "..."
+	tail -n "$lines" "$file"
+fi
+
+
+
+
+
+
+
